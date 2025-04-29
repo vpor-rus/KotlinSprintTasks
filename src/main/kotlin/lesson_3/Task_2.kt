46adc0335af939139491670f3daec255497ee431
Vasiliy <vpor901@gmail.com>
2025-04-29 15:03:21 +1100

    KST-3-2 change name

--- src/main/kotlin/lesson_3/Task_2.kt
+++ src/main/kotlin/lesson_3/Task_2.kt
@@ -2,23 +2,24 @@ package org.example.lesson_3
 
 /*Девушка по имени Андреева Татьяна Сергеевна в 22 года вышла замуж за Сидорова,
 и поменяла фамилию. О ней есть запись в одном из государственных приложений.
- Приложение отслеживает изменения персональных данных граждан.
+Приложение отслеживает изменения персональных данных граждан.
 
-Выведи в консоль данные на момент когда ей было 20
-и на момент когда ей было 22 в формате [ФИО, возраст].
-Все части ФИО и возраст изначально хранятся в отдельных переменных.*/
+Выведи в консоль данные на момент когда ей было 20 и на момент когда ей было 22 в формате
+[ФИО, возраст]. Все части ФИО и возраст изначально хранятся в отдельных переменных.*/
 
 fun main () {
-
+    val firstNameMiss = "Андреева"
+    val firstNameMissis = "Сидорова"
+    val secondName = "Татьяна"
+    val middleName = "Сергеевна"
     var age: Int
 
- age = 20
-    println("$FIRST_NAME_MISS $SECOND_NAME $MIDDLE_NAME $age")
+    age = 20
+    println("$firstNameMiss $secondName $middleName $age лет")
 
- age =22
-    println("$FIRST_NAME_MISSIS $SECOND_NAME $MIDDLE_NAME $age")
+    age =22
+    println("$firstNameMissis $secondName $middleName $age лет")
 }
-val SECOND_NAME = "Татьяна"
-val MIDDLE_NAME = "Сергеевна"
-val FIRST_NAME_MISS = "Андреева"
-val FIRST_NAME_MISSIS = "Сидорова"
+
+
+
