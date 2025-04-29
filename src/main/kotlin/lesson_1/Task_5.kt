0aa41d0d06625c547c651599ed3189e573eb4784
Vasiliy <vpor901@gmail.com>
2025-04-29 19:01:09 +1100

    KS-2-5 income

--- src/main/kotlin/lesson_1/Task_5.kt
+++ src/main/kotlin/lesson_1/Task_5.kt
@@ -13,18 +13,13 @@ import java.text.DecimalFormat
 */
 
 fun main () {
-    const val second = 6_480
+    val second = 6_480
     val minutes = second / MINUTE_VS_SECOND_AND_MINURES_VS_HOUR
     val hours = second / HOUR_VS_SECOND
     val remainingSeconds = second - (minutes * MINUTE_VS_SECOND_AND_MINURES_VS_HOUR)
     val remainingMinutes = minutes - (hours * MINUTE_VS_SECOND_AND_MINURES_VS_HOUR)
 
-    val decimalFormat = DecimalFormat("#00")
-
-    println ("${decimalFormat.format(hours)}:${decimalFormat.format(remainingMinutes)}:${
-        decimalFormat.format(remainingSeconds)}")
-
-        //println("%02d:%02d:%02d".format(hours, minutes, seconds))
+    println("%02d:%02d:%02d".format(hours, remainingMinutes, remainingSeconds))
 }
 
 const val MINUTE_VS_SECOND_AND_MINURES_VS_HOUR = 60
