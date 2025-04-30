dff98c7dbd20b34f43b926a7097c11808431e92b
Vasiliy <vpor901@gmail.com>
2025-04-30 14:06:51 +1100

    KS-1-4 type varible

--- src/main/kotlin/lesson_1/Task_4.kt
+++ src/main/kotlin/lesson_1/Task_4.kt
@@ -25,5 +25,10 @@ package org.example.lesson_1
 Апогей орбиты – 327000*/
 
 fun main() {
-
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6_480
+    val partOfYear: Double = 2.0547945205479453E4
+    val apogee: Int = 327_000
 }
