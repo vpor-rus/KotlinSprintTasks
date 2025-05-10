490e9d06373e1d506f0f0daa6a8bbf907b00302f
Vasiliy <vpor901@gmail.com>
2025-05-10 17:28:14 +1100

    KS-7-5 difficult password

--- src/main/kotlin/lesson_7/Task_5.kt
+++ src/main/kotlin/lesson_7/Task_5.kt
@@ -9,18 +9,30 @@ package org.example.lesson_7
 – минимальная длина пароля 6 символов.*/
 
 fun main() {
-    val upperAlphabeticSymbols = ('A'..'Z').random()
-    val lowerAlphabeticSymbols = ('a'..'z').random()
-    val numberSymbols = ('0'..'9').random()
-    var oneSymbol: String
-    var passwordEdit=mutableListOf<String>()
+    val upperAlphabeticSymbols = ('A'..'Z').toList().joinToString(separator = "")
+    val lowerAlphabeticSymbols = ('a'..'z').toList().joinToString(separator = "")
+    val numberSymbols = ('0'..'9').toList().toList().joinToString(separator = "")
+
 
     println("Введите необходимую длину пароля.\n Напоминаю, не менее 6 символов")
     val lenghtPassword = readln().toInt()
-
-    for (i in 1 .. lenghtPassword) {
-        oneSymbol = ("$upperAlphabeticSymbols$lowerAlphabeticSymbols$numberSymbols").random().toString()
-     passwordEdit.add(oneSymbol)
+    var editPassword = mutableListOf<Char>()
+    //ниже набор обяхательных 6ти символов
+    for (i in 1..2) {
+        val firstSimbol = upperAlphabeticSymbols.random()
+        editPassword.add(firstSimbol)
+        val secondSimbol = lowerAlphabeticSymbols.random()
+        editPassword.add(secondSimbol)
+        val thirdSimbol = numberSymbols.random()
+        editPassword.add(thirdSimbol)
     }
-    passwordEdit.forEach { it: String -> print(it) }
+    //донабор символов более 6ти
+    for (i in 1..(lenghtPassword - REQUIRED_MINIMUM_SYMBOLS)) {
+        editPassword.add(
+            (upperAlphabeticSymbols + lowerAlphabeticSymbols + numberSymbols).random()
+        )
+    }
+    println(editPassword.joinToString(separator = ""))
 }
+
+const val REQUIRED_MINIMUM_SYMBOLS = 6
