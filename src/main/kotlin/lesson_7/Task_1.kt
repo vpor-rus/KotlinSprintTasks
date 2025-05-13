978aeb3ec53ce316320c91ecdd5d855d7c9742ce
Vasiliy <vpor901@gmail.com>
2025-05-13 21:11:16 +1100

    KS-7-1 simple password

--- src/main/kotlin/lesson_7/Task_1.kt
+++ src/main/kotlin/lesson_7/Task_1.kt
@@ -1,26 +1,24 @@
 package org.example.lesson_7
 
-import kotlin.random.Random
-
 /*Создай программу, которая генерирует шестизначные пароли. Пусть пароль будет состоять из чередующихся цифр
  и строчных букв латинского алфавита (без специальных символов). Например, k5p6m2. Выведи сгенерированный
  пароль в консоль.*/
 
 fun main() {
-    val randomAlphabet = ('a'..'z').toList().joinToString(separator = "")
-    val randomeNumber = (1..9).toList().joinToString(separator = "")
+    val setAlphabet = 'a'..'z'
+    val setNumber = 0..9
 
     var counter = 3
-    var characterSet = mutableListOf<Char>()
+    val characterSet = mutableListOf<Char>()
 
     do {
-        val randomeOne = randomAlphabet.random()
+        val randomeOne = setAlphabet.random()
         characterSet.add(randomeOne)
-        val randomeTwo = randomeNumber.random()
+        val randomeTwo = setNumber.random()
         characterSet.add(randomeTwo)
         counter--
     } while (counter > 0)
 
     val editPassword = characterSet.joinToString(separator = "")
-    println("$editPassword")
+    println(editPassword)
 }
