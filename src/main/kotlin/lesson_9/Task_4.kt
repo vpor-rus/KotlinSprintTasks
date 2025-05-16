309bb77dd8c9df095d97129781d14aeb36fba031
Vasiliy <vpor901@gmail.com>
2025-05-16 22:04:34 +1100

    KS-9-4 five ingredients in list

--- src/main/kotlin/lesson_9/Task_4.kt
+++ src/main/kotlin/lesson_9/Task_4.kt
@@ -1,5 +1,8 @@
 package org.example.lesson_9
 
+import kotlin.collections.sorted
+
+
 /*Напиши программу, которая один раз запрашивает у пользователя пять названий
 ингредиентов блюда в одной строке, разделяя их запятыми (с пробелом).
 
@@ -10,7 +13,10 @@ package org.example.lesson_9
 
 fun main() {
 
-    print("Введи 5 ингредиентов, перечисленных через “, ” (запятая с пробелом)")
+    print("Введи 5 ингредиентов, перечисленных через “, ” (запятая с пробелом): ")
     val newFiveIngredients = readln()
-
+    val listFiveIngredients = newFiveIngredients.split(", ")
+    val sortedListFiveIngredients = listFiveIngredients.sorted()
+    println(sortedListFiveIngredients)
 }
+
