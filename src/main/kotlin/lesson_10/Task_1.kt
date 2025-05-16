8588201b7843e5082e173551592fe2f719108c00
Vasiliy <vpor901@gmail.com>
2025-05-17 08:15:24 +1100

    KS-10-1 rundom comparing number

---
+++ src/main/kotlin/lesson_10/Task_1.kt
@@ -0,0 +1,32 @@
+package org.example.lesson_10
+
+/*Напиши небольшую игру, в которой пользователь и компьютер по очереди бросают кости.
+ Побеждает выбросивший наибольшее число.
+
+- поочередно выводи сообщение о ходе игрока и компьютера (отображать кто бросил, какие значения на кубиках);
+- бросок кости вынеси в отдельную функцию, в которой генерируется случайное число от 1 до 6;
+- в отдельные переменные сохрани результат работы функции для игрока и компьютера;
+- после сравнения результатов, выведи соответствующее сообщение в консоль. Например: "Победило человечество",
+"Победила машина" или "Победила дружба".*/
+
+fun main() {
+    print("ход игрока: ")
+    val stepPlayer = rundomNumber()
+    println(stepPlayer)
+    print("ход компъютера:  ")
+    val stepComputer = rundomNumber()
+    println(stepComputer)
+    resultComparingNumbers(stepPlayer, stepComputer)
+}
+
+fun rundomNumber(): Int? = (0..9).random()
+
+fun resultComparingNumbers(stepPlayer: Int?, stepComputer: Int?) {
+    if (stepComputer > stepPlayer) {
+        println("Победила машина")
+    }else if (stepComputer < stepPlayer) {
+        println("Победило человечество")
+    } else {
+        println("Победила дружба")
+    }
+}
