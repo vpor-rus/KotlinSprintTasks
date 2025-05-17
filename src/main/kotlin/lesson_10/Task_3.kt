9d8689842afa242d3240cf3d89f0c280be8d4324
Vasiliy <vpor901@gmail.com>
2025-05-17 15:03:14 +1100

    KS-10-3 password number and special simbols

---
+++ src/main/kotlin/lesson_10/Task_3.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_10
+
+/*Напиши программу, которая генерирует пароли. Пароль должен состоять из последовательно
+чередующихся цифр и специальных символов. Например, 4#4%2!.
+
+- пользователь сам задает длину пароля;
+- для генерации пароля должна быть отдельная функция, принимающая целочисленное значение длины пароля
+ и возвращающая готовый пароль;
+- пароль должен содержать цифры от 0 до 9, специальные символы: !"#$%&'()*+,-./ и пробел
+(всего 16 спецсимволов).*/
+
+fun main() {
+    print("Введите необходимую длину пароля")
+    val numberSymbolsOfPassword = readln().toInt()
+
+}
+
+fun randomNumber(): Char? = ('0'..'9').random()
+
+fun randomSymbols(): Char? = (('!'..'/') + ' ').random()
+
+fun stringSymbolsOfPassword(symbolPassword: Char?, numberPassword: Char?, numberSymbolsOfPassword: Int?) {
+   val listSimbols
+    for (i in 1 ..numberSymbolsOfPassword) {
+        if (i % 2 != 0) {
+            numberPassword
+        }
+    }
+}
