ce607537b3ead8751085833143ffc5162cc41a85
Vasiliy <vpor901@gmail.com>
2025-05-21 07:43:48 +1100

    KS-11-2 password manipulation

--- src/main/kotlin/lesson_11/Task_2.kt
+++ src/main/kotlin/lesson_11/Task_2.kt
@@ -22,3 +22,35 @@ fun main() {
     changeUser.changePassword()
     changeUser.userInformation()
 }
+
+class User2 (
+    val id: Int,
+    val login: String,
+    var password: String,
+    val emailAdress: String,
+    var bio: String? = null,
+) {
+    fun userInformation() {
+        println("User2 id: $id")
+        println("User2 login: $login")
+        println("User2 password: $password")
+        println("User2 email adress: $emailAdress")
+        if (bio != null) {
+            println("User2 bio: $bio")
+        }
+    }
+
+    fun fillingInBio() {
+        bio = readln()
+    }
+
+    fun changePassword() {
+        print("Введите пароль для подтверждения: ")
+        val examinerPassword = readln()
+        if (examinerPassword == password) {
+            print("Введите новый пароль: ")
+            password = readln()
+            println("Пароль изменен")
+        }
+    }
+}
