17ac0c5adb3bc394577533f34a851735e4e4953c
Vasiliy Portko <vpor901@gmail.com>
2025-05-21 19:19:02 +1100

    KS-11-4 pecipe app

---
+++ src/main/kotlin/lesson_11/Task_4.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_11
+
+/*Дизайнер предоставил макет Android-приложения:
+https://www.figma.com/file/89K5JomHGyIkUFUaBKRBXi/RecipesApp?type=design&node-id=1%3A181&mode=design&t=DsOaqqRfAeCTY2eN-1
+
+Для представленных экранов спроектируй следующие сущности для хранения данных:
+
+- Категория рецепта
+- Рецепт
+- Ингредиент*/
+
+fun main() {
+
+}
+
+class RecipeCategory (
+
+)
