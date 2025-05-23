86ebf76c49f5314ccc219f809b0c06edcfb1357a
Vasiliy Portko <vpor901@gmail.com>
2025-05-24 07:11:02 +1100

    KS-12-4 kelvin to celsium automat

--- src/main/kotlin/lesson_12/Task_4.kt
+++ src/main/kotlin/lesson_12/Task_4.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_12
 
-/*Измени программу таким образом, чтобы информация о погоде выводилась в консоль сразу при создании объекта.
+/*Измени программу таким образом, чтобы информация о погоде выводилась в консоль сразу при создании объекта.*/
 
 fun main() {
 val tursdayWeather = DailyTemperature(
@@ -9,19 +9,10 @@ val tursdayWeather = DailyTemperature(
 )
 }
 
-class DailyTemperature(val _dayTemperature: Int, val _nightTemperature: Int): String {
-    val dayTemperatureKelvin = _dayTemperature
-    val nightTemperatureKelvin = _nightTemperature
-    val celsiumDayTemperature = kelvinToCelsium(dayTemperatureKelvin)
-    val celsiumNightTemperature = kelvinToCelsium(nightTemperatureKelvin)
-    val edit = "дневная температура: $celsiumDayTemperature\n" +
-            "ночная температураЖ $celsiumNightTemperature"
-    return "$edit"
-}
+class DailyTemperature(val dayTemperature: Int, val nightTemperature: Int): String
 
 fun kelvinToCelsium(temperatureKelvin: Int): Int {
     return temperatureKelvin - KELVIN_TO_CELSIUM
 }
 
 const val KELVIN_TO_CELSIUM = 275
- */
