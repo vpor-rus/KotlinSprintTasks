8ee621e58880eb6ef64cb59e05def58378566176
Vasiliy Portko <vpor901@gmail.com>
2025-05-23 22:09:25 +1100

    KS-11-5 create forum

--- src/main/kotlin/lesson_11/Task_5.kt
+++ src/main/kotlin/lesson_11/Task_5.kt
@@ -19,16 +19,45 @@ package org.example.lesson_11
 каждого пользователя.*/
 
 fun main() {
+    val forum = UserInForumBuilder().apply {
+       setUserName("Телепузик")
+    }.build()
 
+    val messageInForum = MessageForumBuilder().apply {
+                setMessage("Привет вам из 2025")
+    }.build()
+
+    println("$forum : $messageInForum")
 }
 
 class MessageForum(val autorId: Int, val message: String)
 
-class UserInForum(val userId: Int, val userName: Int)
+class MessageForumBuilder {
+    var autorId: Int = 0
+    var message: String = ""
 
-class Forum(_messageForum: MessageForum, _userInForum: UserInForum) {
+    fun setAutorId(autorId: Int) = apply { this.autorId = autorId }
+    fun setMessage(message: String) = apply { this.message = message }
 
+    fun build() = MessageForum(autorId, message)
 }
+
+class UserInForum(val userId: Int, val userName: String)
+
+class UserInForumBuilder {
+    var userId: Int = 0
+    var userName: String = ""
+
+    fun setUserId(userId: Int) = apply { this.userId = userId }
+    fun setUserName(userName: String) = apply { this.userName = userName }
+
+    fun build() = UserInForum(userId, userName)
+}
+// У меня в результатах абракадабра:
+// org.example.lesson_11.UserInForum@66a29884 : org.example.lesson_11.MessageForum@4769b07b
+// Что прочитать что бы разобраться?
+// Я понимаю что это адреса ячеек, не могу понять как вывести их значения.
+
 
 
 
